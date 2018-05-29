ef74316ea339ae1ab1ba95ec9ef244aef9703682
Cynthia <172587349@qq.com>
2018-05-29 13:11:04 +0800

    成功改了项目名字

--- src/Cynthia.Chat.Server/Program.cs
+++ src/Cynthia.Chat.Server/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Cynthia.Test.Chat
+namespace Cynthia.Chat.Server
 {
     public class Program
     {
