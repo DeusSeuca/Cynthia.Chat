31a574c441c0747416bd47e4e4b5071ed1af4a8a
Cynthia <172587349@qq.com>
2018-05-30 23:58:25 +0800

    完善了客户端

--- src/Cynthia.Chat.Client/Program.cs
+++ src/Cynthia.Chat.Client/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Cynthia.Chat.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Utilities;
 
 namespace Cynthia.Chat.Client
 {
@@ -7,9 +12,31 @@ namespace Cynthia.Chat.Client
     {
         static void Main(string[] args)
         {
+            string userName;
+            string context;
+            int count = 0;
+            Console.WriteLine("请输入在聊天室的名称~:");
+            userName = Console.ReadLine();
             DataProcessing client = new DataProcessing();
-            client.SendData("格子","我才不是傲娇哼呢");
-            Console.WriteLine(JsonConvert.SerializeObject(client.GetData(0)));
+            client.Url = "http://cynthia.ovyno.com/api/chat/";
+            Test(count, client);
+            while (true)
+            {
+                context = Console.ReadLine();
+                client.SendData(userName, context);
+            }
+        }
+        public static async void Test(int count, DataProcessing client)
+        {
+            while (true)
+            {
+                count += PutData(await client.GetData(count));
+                await Task.Delay(1000);
+            }
+        }
+        public static int PutData(IEnumerable<JsonData> data)
+        {
+            return data.Select(d => $"{d.Name}  {d.Time}\n {d.Content}\n---------------------------------").ForAll(Console.WriteLine).Count();
         }
     }
 }
