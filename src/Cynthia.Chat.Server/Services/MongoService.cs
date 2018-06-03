d983b7b5f57d813a22b6307131bfc391ca9087a5
Cynthia <172587349@qq.com>
2018-06-04 04:40:12 +0800

    回滚一部分,进行重新测试

--- src/Cynthia.Chat.Server/Services/MongoService.cs
+++ src/Cynthia.Chat.Server/Services/MongoService.cs
@@ -33,27 +33,16 @@ namespace Cynthia.Chat.Server.Services
         }
         public void SaveData()
         {
-            //测试用
-            var url = "mongodb://cynthia.ovyno.com:27017";
-            var client = new MongoClient(url);
-            var db = client.GetDatabase("chat");
-            var collection = db.GetCollection<JsonData>("test");
-            //var collection = Client.GetDatabase(dataBaseName).GetMongoCollection<JsonData>(collectionName);
+            var collection = Client.GetDatabase(dataBaseName).GetMongoCollection<JsonData>(collectionName);
             Data.GetData(0).Skip(_strategy).ForAll(x => collection.InsertOne(x));
             collection.InsertOne(new JsonData { Name = "测试数据", Content = "如果数据库出现这条数据,代表上面的语句错误", Time = DateTime.Now, Id = Guid.NewGuid() });
-            //_strategy = Data.Count;
+            _strategy = Data.Count;
         }
         public IEnumerable<JsonData> GetEndData(int count)
         {
-            //获得默认的count条数据
-            //测试用
-            var url = "mongodb://cynthia.ovyno.com:27017";
-            var client = new MongoClient(url);
-            var db = client.GetDatabase("chat");
-            var collection = db.GetCollection<JsonData>("test");
-            //var collection = Client.GetDatabase(dataBaseName).GetMongoCollection<JsonData>(collectionName);
+            var collection = Client.GetDatabase(dataBaseName).GetMongoCollection<JsonData>(collectionName);
             var data = collection.AsQueryable<JsonData>().OrderBy(x => x.Time).Reverse().Take(count).Reverse().AsEnumerable();
-            //_strategy = data.Count();
+            _strategy = data.Count();
             return data;
         }
     }
